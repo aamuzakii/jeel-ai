e5d9851b09124eb0c89b0c7087dc61c854ac29e8
Muzaki新联想 <aamuzakii@gmail.com>
2024-02-10 22:10:44 +0700

    add css watcher

--- mlforkids-api/web-watcher.js
+++ mlforkids-api/web-watcher.js
@@ -1,6 +1,36 @@
 const chokidar = require('chokidar');
 const { exec } = require('child_process');
+const fs = require('fs');
+const path = require('path');
 
+function getAllCssFiles(directoryPath) {
+  const cssFiles = [];
+
+  function traverseDirectory(currentPath) {
+    const files = fs.readdirSync(currentPath);
+
+    files.forEach(file => {
+      const filePath = path.join(currentPath, file);
+      const stats = fs.statSync(filePath);
+
+      if (stats.isDirectory()) {
+        traverseDirectory(filePath); // Recursive call for nested directories
+      } else if (stats.isFile() && path.extname(file) === '.css') {
+        const relativePath = path.relative(directoryPath, filePath);
+        cssFiles.push('public/' + relativePath);
+      }
+    });
+  }
+
+  traverseDirectory(directoryPath);
+  return cssFiles;
+}
+
+// Example usage:
+const folderPath = '/home/zaki/freelance/taxinomitis-ml/mlforkids-api/public';
+const cssFiles = getAllCssFiles(folderPath);
+
+console.log(cssFiles);
 
 // Initialize languageWatcher.
 const languageWatcher = chokidar.watch(['public/languages'], {
@@ -18,6 +48,28 @@ function handleChange(path) {
 
   // Run the custom Bash script.
   exec(`npx gulp languages`, (error, stdout, stderr) => {
+    if (error || stderr) {
+      console.error(`Error running the script: ${error} ${stderr}`);
+      return;
+    }
+    console.log(`Script output: ${stdout}`);
+  });
+}
+
+const cssWatcher = chokidar.watch(['public/app.css'], {
+  persistent: true,
+});
+
+// Add event listeners.
+cssWatcher
+  .on('add', (path) => handleChangeGlobalCSS(path))
+  .on('change', (path) => handleChangeGlobalCSS(path));
+
+function handleChangeGlobalCSS(path) {
+  console.log(`File ${path} has been changed. Running the script...`);
+
+  // Run the custom Bash script.
+  exec(`npx gulp css`, (error, stdout, stderr) => {
     if (error || stderr) {
       console.error(`Error running the script: ${error} ${stderr}`);
       return;
