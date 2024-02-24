07059f618f78e1baa4e5d0b1f6c3b995d44954d2
Muzaki新联想 <aamuzakii@gmail.com>
2024-02-24 19:47:59 +0700

    infinite-reload fixed

--- mlforkids-api/public/auth0-dev-variables.js
+++ mlforkids-api/public/auth0-dev-variables.js
@@ -1,7 +1,7 @@
 var AUTH0_CLIENT_ID='ztvKHoFKRh77os8QILVCpXLTQbz8nn2C';
 var AUTH0_DOMAIN='dev-q28narhg3zzoyq0a.us.auth0.com';
 var AUTH0_CUSTOM_DOMAIN='dev-q28narhg3zzoyq0a.us.auth0.com';
-var AUTH0_CDN_BASE='https://cdn.auth0.com';
+var AUTH0_CDN_BASE;
 var AUTH0_CALLBACK_URL=window.location.href;
-var AUTH0_AUDIENCE='http://localhost:3000/users';
-var REQUESTED_SCOPES='openid profile email write:training';
+var AUTH0_AUDIENCE='audience';
+var REQUESTED_SCOPES='openid email write:training';
