71bd2d4206b28a6e9b195e006df26f83e920b002
Muzaki新联想 <aamuzakii@gmail.com>
2024-02-26 02:53:27 +0700

    fix AUTH0_AUDIENCE

--- mlforkids-api/public/auth0-dev-variables.js
+++ mlforkids-api/public/auth0-dev-variables.js
@@ -3,5 +3,5 @@ var AUTH0_DOMAIN='dev-dabiad2n71gvvkx3.us.auth0.com';
 var AUTH0_CUSTOM_DOMAIN='dev-dabiad2n71gvvkx3.us.auth0.com';
 var AUTH0_CDN_BASE;
 var AUTH0_CALLBACK_URL=window.location.href;
-var AUTH0_AUDIENCE='audience';
+var AUTH0_AUDIENCE='https://machinelearning.temanumroh.com/users';
 var REQUESTED_SCOPES='openid email write:training';
